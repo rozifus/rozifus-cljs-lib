6b8dbf1de02378869b91c15e53b6f36ab1b924e7
Ryan Miller <rozifus@gmail.com>
2016-04-11 21:25:14 +1000

    rename files to not use hyphens

--- src/leiningen/new/rozifus_cljs_lib.clj
+++ src/leiningen/new/rozifus_cljs_lib.clj
@@ -9,9 +9,9 @@
               :sanitized (name-to-path name)}]
     (->files data
       ["project.clj" (render "project.clj" data)]
-      ["src/{{sanitized}}/core.cljs" (render "src_core.cljs" data)]
-      ["test/{{sanitized}}/core-test.cljs" (render "test_core-test.cljs" data)]
-      ["test/{{sanitized}}/runner.cljs" (render "test_runner.cljs" data)]
+      ["src/{{sanitized}}/core.cljs" (render "src__core.cljs" data)]
+      ["test/{{sanitized}}/core_test.cljs" (render "test__core_test.cljs" data)]
+      ["test/{{sanitized}}/runner.cljs" (render "test__runner.cljs" data)]
       ["README.md" (render "README.md" data)]
       ["LICENSE" (render "LICENSE" data)]
       [".gitignore" (render "gitignore" data)])))
